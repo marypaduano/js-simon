e2a6a2e89d2142e71d6f8668bdd7750d7bd73999
marypaduano <maria.paduano@gmail.com>
2023-02-14 14:00:35 +0100

    FIX result game

--- js/script.js
+++ js/script.js
@@ -5,40 +5,42 @@ let start = document.getElementById('start')
 start.addEventListener('click', function () {
 
   let userNumList = []
-  let randomNumList = []
-  let randomNum
-  let userNum
-
-  for (let i = 0; i < 5; i++) {
-    randomNum = randomNumGenerate(1, 100)
-    console.log(randomNum)
-    randomNumList.push(randomNum)
-  }
-  console.log(randomNumList)
+  let randomNumList = numGenerate(5, 1, 100)
+  let guessNum = []
+  
   alert("Memorizza questi numeri: " + randomNumList)
+  console.log(randomNumList)
 
   setTimeout(function () {
-
-    for (let i = 0; i < 5; i++) {
-      numeroPrompt = parseInt(prompt("inserisci i numeri visti poco fa"))
-      for (let a = 0; a < randomNumList.length; a++) {
-        if (userNum == randomNumList[a]) {
-          userNumList.push(userNum)
-        }
+    while (userNumList.length < randomNumList.length) {
+      let userNum = parseInt(prompt("inserisci i numeri visti poco fa"))
+      if (randomNumList.includes(userNum) && !userNumList.includes(userNum)) {
+        guessNum.push(userNum)
       }
 
+      userNumList.push(userNum)
+
     }
-    alert("Hai indovinato i numeri: " + userNumList)
-    console.log(userNumList)
+    alert("Hai indovinato " + guessNum.length + 'numeri:  I numeri indovinati sono questi: ' + guessNum)
+    console.log(guessNum)
 
   }, 3000)
 
 
 })
 
-
-
-
-function randomNumGenerate(min, max) {
+function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function numGenerate(length, min, max) {
+	let randomNumList = []
+
+	while (randomNumList.length < length) {
+		const userNum = randomNum(min, max)
+		if (!randomNumList.includes(userNum)) {
+			randomNumList.push(userNum)
+		}
+	}
+	return randomNumList
 }
